b9308230ada02afb86b9667f6aad84217865df75
aadne mauno <aadneom@aadneom-laptop.aadneom>
2025-03-04 17:59:41 +0100

    [WIP][packages/ctk] Removed CTKdata dependency

--- guix-systole/packages/ctk.scm
+++ guix-systole/packages/ctk.scm
@@ -58,54 +58,58 @@
                 #:configure-flags
                 (list         
                     ;"DCMAKE_CXX_COMPILER:FILEPATH=${CMAKE_CXX_COMPILER}"
-                    ;" -DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}"
-                    ;" -DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}"
-                    ;" -DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}"
-                    ;" -DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD}"
-                    ;" -DCMAKE_CXX_STANDARD_REQUIRED:BOOL=${CMAKE_CXX_STANDARD_REQUIRED}"
-                    ;" -DCMAKE_CXX_EXTENSIONS:BOOL=${CMAKE_CXX_EXTENSIONS}"
-                    ;" -DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}"
-                    ;" -DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}"
-                    " -DBUILD_TESTING:BOOL=OFF"
-                    ;" -DCTK_BUILD_QTDESIGNER_PLUGINS:BOOL=${Slicer_BUILD_QT_DESIGNER_PLUGINS}"
-                    ;" -DCTK_INSTALL_BIN_DIR:STRING=${Slicer_INSTALL_BIN_DIR}"
-                    ;" -DCTK_INSTALL_LIB_DIR:STRING=${Slicer_INSTALL_LIB_DIR}"
-                    ;" -DCTK_INSTALL_QTPLUGIN_DIR:STRING=${Slicer_INSTALL_QtPlugins_DIR}"
+                    ;"-DCMAKE_CXX_FLAGS:STRING=${ep_common_cxx_flags}"
+                    ;"-DCMAKE_C_COMPILER:FILEPATH=${CMAKE_C_COMPILER}"
+                    ;"-DCMAKE_C_FLAGS:STRING=${ep_common_c_flags}"
+                    ;"-DCMAKE_CXX_STANDARD:STRING=${CMAKE_CXX_STANDARD}"
+                    ;"-DCMAKE_CXX_STANDARD_REQUIRED:BOOL=${CMAKE_CXX_STANDARD_REQUIRED}"
+                    ;"-DCMAKE_CXX_EXTENSIONS:BOOL=${CMAKE_CXX_EXTENSIONS}"
+                    ;"-DADDITIONAL_C_FLAGS:STRING=${ADDITIONAL_C_FLAGS}"
+                    ;"-DADDITIONAL_CXX_FLAGS:STRING=${ADDITIONAL_CXX_FLAGS}"
+                    "-DBUILD_TESTING:BOOL=OFF"
+                    ;"-DCTK_BUILD_QTDESIGNER_PLUGINS:BOOL=${Slicer_BUILD_QT_DESIGNER_PLUGINS}"
+                    ;"-DCTK_INSTALL_BIN_DIR:STRING=${Slicer_INSTALL_BIN_DIR}"
+                    ;"-DCTK_INSTALL_LIB_DIR:STRING=${Slicer_INSTALL_LIB_DIR}"
+                    ;"-DCTK_INSTALL_QTPLUGIN_DIR:STRING=${Slicer_INSTALL_QtPlugins_DIR}"
 
-                    " -DCTK_USE_GIT_PROTOCOL:BOOL=OFF"  ; turn off git protocol, as it is not supported by GitHub
+                    ; ------------ Disabling CTKData as its only used for testing ------------
 
-                    ;" -DCTK_USE_SYSTEM_VTK:BOOL=${CTK_USE_SYSTEM_VTK}"
-                    ;" -DVTK_DIR:PATH=${VTK_DIR}"
-                    ;" -DCTK_USE_SYSTEM_ITK:BOOL=${CTK_USE_SYSTEM_ITK}"
-                    ;" -DITK_DIR:PATH=${ITK_DIR}"
-                    " -DCTK_LIB_Widgets:BOOL=ON"
-                    " -DCTK_LIB_Visualization/VTK/Widgets:BOOL=ON"
-                    " -DCTK_LIB_Visualization/VTK/Widgets_USE_TRANSFER_FUNCTION_CHARTS:BOOL=ON"
-                    " -DCTK_LIB_ImageProcessing/ITK/Core:BOOL=ON"
-                    " -DCTK_LIB_PluginFramework:BOOL=OFF"
-                    " -DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF"
-                    ;" -DCTK_APP_ctkDICOM:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
-                    ;" -DCTK_LIB_DICOM/Core:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
-                    ;" -DCTK_LIB_DICOM/Widgets:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
-                    ;" -DCTK_USE_QTTESTING:BOOL=${Slicer_USE_QtTesting}"
-                    ;" -DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}"
+                    "-DCTK_ENABLE_CTKDATA:BOOL=OFF"
+
+                    "-DCTK_USE_GIT_PROTOCOL:BOOL=OFF"  ; turn off git protocol, as it is not supported by GitHub
+
+                    ;"-DCTK_USE_SYSTEM_VTK:BOOL=${CTK_USE_SYSTEM_VTK}"
+                    ;"-DVTK_DIR:PATH=${VTK_DIR}"
+                    ;"-DCTK_USE_SYSTEM_ITK:BOOL=${CTK_USE_SYSTEM_ITK}"
+                    ;"-DITK_DIR:PATH=${ITK_DIR}"
+                    "-DCTK_LIB_Widgets:BOOL=ON"
+                    "-DCTK_LIB_Visualization/VTK/Widgets:BOOL=ON"
+                    "-DCTK_LIB_Visualization/VTK/Widgets_USE_TRANSFER_FUNCTION_CHARTS:BOOL=ON"
+                    "-DCTK_LIB_ImageProcessing/ITK/Core:BOOL=ON"
+                    "-DCTK_LIB_PluginFramework:BOOL=OFF"
+                    "-DCTK_PLUGIN_org.commontk.eventbus:BOOL=OFF"
+                    ;"-DCTK_APP_ctkDICOM:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
+                    ;"-DCTK_LIB_DICOM/Core:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
+                    ;"-DCTK_LIB_DICOM/Widgets:BOOL=${Slicer_BUILD_DICOM_SUPPORT}"
+                    ;"-DCTK_USE_QTTESTING:BOOL=${Slicer_USE_QtTesting}"
+                    ;"-DGIT_EXECUTABLE:FILEPATH=${GIT_EXECUTABLE}"
 
                     ; ---------------------- PythonQt wrapping ----------------------
 
-                    ;" -DCTK_LIB_Scripting/Python/Core:BOOL=${Slicer_USE_PYTHONQT}"  
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_USE_VTK:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTCORE:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTGUI:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTUITOOLS:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTNETWORK:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTMULTIMEDIA:BOOL=${_wrap_qtmultimedia}"
-                    ;" -DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTWEBKIT:BOOL=${_wrap_qtwebkit}"
-                    ;" -DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer_USE_PYTHONQT}"
-                    ;" -DCTK_ENABLE_Python_Wrapping:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core:BOOL=${Slicer_USE_PYTHONQT}"  
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_USE_VTK:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTCORE:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTGUI:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTUITOOLS:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTNETWORK:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTMULTIMEDIA:BOOL=${_wrap_qtmultimedia}"
+                    ;"-DCTK_LIB_Scripting/Python/Core_PYTHONQT_WRAP_QTWEBKIT:BOOL=${_wrap_qtwebkit}"
+                    ;"-DCTK_LIB_Scripting/Python/Widgets:BOOL=${Slicer_USE_PYTHONQT}"
+                    ;"-DCTK_ENABLE_Python_Wrapping:BOOL=${Slicer_USE_PYTHONQT}"
 
                     ; ---------------------------------------------------------------
 
-                    ;" ${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}"
+                    ;"${EXTERNAL_PROJECT_OPTIONAL_CMAKE_CACHE_ARGS}"
                 )
             )
         )
@@ -114,14 +118,15 @@
             (list
                 qtbase-5
                 qttools-5  ; Use qttools-5 instead of qttools (which would be Qt6)
+                qtsvg-5
             )
         )
 
-        ;(native-inputs
-        ;    (list
-        ;        git
-        ;    )    
-        ;)
+        (native-inputs
+            (list
+                git
+            )    
+        )
 
         ; ---------------------------- REQUIRED INFO FIELDS -----------------------------
 
