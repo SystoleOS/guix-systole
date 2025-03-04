4e8f686128ddda3c63936d222ccc4f9d4d33cf5c
Omar El Hajj <omarelhajj97@gmail.com>
2025-03-04 18:26:36 +0000

    [WIP][packages/qRestAPI] include build flags and inputs

--- guix-systole/packages/qrestapi.scm
+++ guix-systole/packages/qrestapi.scm
@@ -15,6 +15,18 @@
                      (sha256
                        (base32 "0jfnja3frcm4vkibi1vygdh7f4dmhqxni43bbb3rmlcl6jlyaibl"))))
                  (build-system cmake-build-system)
+                 (arguments
+                   (list
+                     #:build-type "Release"
+                     #:configure-flags
+                     #~'(; Explicitly use Qt version 5
+                         "-DqRestAPI_QT_VERSION:STRING=5"
+                         "-DBUILD_SHARED_LIBS:BOOL=ON"
+                         "-DBUILD_TESTING:BOOL=OFF"
+                         "-DqRestAPI_STATIC:BOOL=OFF"
+                         ))
+                 (inputs (list
+                           qtbase-5))
                  (home-page "https://github.com/commontk/qRestAPI")
                  (synopsis "Simple Qt library allowing to synchronously or asynchronously query a REST server.")
                  (description "qRestAPI is a cross-platform [Qt-based](https://www.qt.io/) library 
@@ -30,4 +42,4 @@
 
                               [girder]: https://github.com/girder/girder
                               [midas]: https://github.com/midasplatform/midas")
-                (license license:asl2.0)))
+                (license license:asl2.0))))
