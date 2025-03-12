75f3dce1eea17cae4bc84964a28950aecc3c3e9e
Khai Duong <duongkhai@gmail.com>
2025-03-12 01:28:10 +0100

    [WIP][packages/slicer] Add Teem to ITK

--- guix-systole/packages/teem.scm
+++ guix-systole/packages/teem.scm
@@ -19,6 +19,21 @@
        (sha256
         (base32 "0y8wwzkflj6v5nx0v8cgzryqlxii0px3mcgb3bff1nhyr5zf9yp1"))))
     (build-system cmake-build-system)
+    (arguments
+     `(#:tests? #f
+       #:configure-flags (list "-DBUILD_SHARED_LIBRS:BOOL=ON"
+       "-DBUILD_TESTING:BOOL=OFF"
+      "-DTeem_USE_LIB_INSTALL_SUBDIR:BOOL=ON" ;; TODO fix error from build log
+      "-DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF"
+      "-DTeem_PTHREAD:BOOL=OFF"
+      "-DTeem_BZIP2:BOOL=OFF"
+      "-DTeem_ZLIB:BOOL=ON"
+      "-DTeem_PNG:BOOL=OFF"
+      ;; -DZLIB_ROOT:PATH=${ZLIB_ROOT}
+      ;; -DZLIB_INCLUDE_DIR:PATH=${ZLIB_INCLUDE_DIR}
+      ;; -DZLIB_LIBRARY:FILEPATH=${ZLIB_LIBRARY}
+      ;; -DTeem_PNG_DLLCONF_IPATH:PATH=${VTK_DIR}/Utilities
+       )))
     (home-page "https://github.com/Slicer/teem/")
     (synopsis
      "A group of libraries for representing, processing and visualizing scientific raster data.")
