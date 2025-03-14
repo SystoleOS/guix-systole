1c2bc7be70e81f461a9706600d2a2a40e3d98372
Khai Duong <duongkhai@gmail.com>
2025-03-14 01:43:12 +0100

    [ENH][packages/vtk] Add vtkaddon package

--- guix-systole/packages/vtk.scm
+++ guix-systole/packages/vtk.scm
@@ -1,16 +1,32 @@
 (define-module (guix-systole packages vtk)
   #:use-module ((guix licenses)
-                #:prefix license:) ;)
+                #:prefix license:)
   #:use-module (guix packages)
-  #:use-module (guix gexp)
+  #:use-module (gnu packages algebra)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages xml)
   #:use-module (guix download)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix gexp)
   #:use-module (gnu packages qt)
   #:use-module (gnu packages mpi)
   #:use-module (gnu packages tbb)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
+  #:use-module (gnu packages image)
   #:use-module ((gnu packages image-processing)
-                #:prefix imgproc:))
+                #:prefix imgproc:)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages gl)
+  #:use-module (gnu packages geo)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages maths)
+  #:use-module (gnu packages mpi)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages serialization)
+  #:use-module (gnu packages xiph))
 
 (define-public vtk-slicer
   (package
@@ -56,7 +72,7 @@
           "-DVTK_MODULE_USE_EXTERNAL_VTK_tiff:BOOL=ON"
           "-DVTK_MODULE_USE_EXTERNAL_VTK_zlib:BOOL=ON"
           "-DVTK_MODULE_ENABLE_VTK_RenderingExternal:STRING=YES" ;for F3D
-          "-DVTK_WRAP_PYTHON:BOOL=ON"
+          "-DVTK_WRAP_PYTHON:BOOL=OFF"
           "-DVTK_SMP_ENABLE_TBB:BOOL=ON"
           "-DVTK_USE_MPI:BOOL=ON"
           "-DVTK_USE_TK:BOOL=OFF"
@@ -78,3 +94,56 @@
       #:tests? #f))
     (inputs (modify-inputs (package-inputs imgproc:vtk)
               (append python-pyqt qtbase-5 tbb openmpi)))))
+
+(define-public vtkaddon-slicer
+  (package
+    (name "vtkaddon-slicer")
+    (version "b5aa061")
+    (source
+     (origin
+       (method url-fetch)
+       (uri
+        "https://github.com/Slicer/vtkAddon/archive/b5aa0615a6486b6bdceeb13bd59c2fb9f89cce42.tar.gz")
+       (sha256
+        (base32 "0wazsirav972mxkawfaw0lpnkylxfr19xjrd5s03blr2kid50a91"))
+    (build-system cmake-build-system)
+    (outputs '("out"))
+    (arguments
+     `(#:tests? #f
+       #:configure-flags (list "-DBUILD_SHARED_LIBS:BOOL=ON"
+                               "-DBUILD_TESTING:BOOL=OFF"
+                               "-DvtkAddon_INSTALL_NO_DEVELOPMENT:BOOL=OFF"
+                               ;; "-DvtkAddon_USE_UTF8=ON"
+                               "-DvtkAddon_WRAP_PYTHON=OFF" ;Enable whenever Python support is enabled
+                               "-DVTK_WRAP_PYTHON=OFF"
+                               ;; "-DvtkAddon_LAUNCH_COMMAND:STRING=" ;; Needs testing
+                               "-DCMAKE_BUILD_WITH_INSTALL_RPATH=ON")))
+
+    (inputs (list vtk-slicer
+                  eigen
+                  expat
+                  double-conversion
+                  freetype
+                  gl2ps
+                  glew
+                  libjpeg-turbo
+                  libxml2
+                  jsoncpp
+                  libharu
+                  libtheora
+                  libxml++
+                  lz4
+                  mpich
+                  netcdf
+                  proj
+                  python-3.10
+                  qtbase-5
+                  hdf5))
+
+    (native-inputs (list coreutils))
+    (home-page "https://github.com/Slicer/vtkAddon/")
+    (synopsis
+     "General-purpose features that may be integrated into VTK library in the future.")
+    (description
+     "General-purpose features that may be integrated into VTK library in the future.")
+    (license license:bsd-3)))
