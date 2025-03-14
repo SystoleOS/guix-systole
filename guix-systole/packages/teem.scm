6074ab7eadba2be3af04ee28416957760874a9b7
Khai Duong <duongkhai@gmail.com>
2025-03-14 02:00:36 +0100

    [BUG][packages/teem] Fix missing TeemConfig.cmake
    TeemConfig.cmake was not installed in lib/cmake/Teem/. Instead, it was
    installed in lib/. Also, remove obsolete code.

--- guix-systole/packages/teem.scm
+++ guix-systole/packages/teem.scm
@@ -1,6 +1,8 @@
 (define-module (guix-systole packages teem)
+  #:use-module (guix-systole packages vtk)
   #:use-module (guix build-system cmake)
   #:use-module (guix download)
+  #:use-module (guix gexp)
   #:use-module ((guix licenses)
                 #:prefix license:)
   #:use-module (guix packages)
@@ -17,23 +19,17 @@
        (uri
         "https://github.com/Slicer/teem/archive/e4746083c0e1dc0c137124c41eca5d23adf73bfa.tar.gz")
        (sha256
-        (base32 "0y8wwzkflj6v5nx0v8cgzryqlxii0px3mcgb3bff1nhyr5zf9yp1"))))
+        (base32 "0y8wwzkflj6v5nx0v8cgzryqlxii0px3mcgb3bff1nhyr5zf9yp1"))
+       (patches (list (local-file
+                       "patches/0001-BUG-packages-teem-fix-missing-teem-config-cmake.patch")
+                      ))))
     (build-system cmake-build-system)
     (arguments
      `(#:tests? #f
-       #:configure-flags (list "-DBUILD_SHARED_LIBRS:BOOL=ON"
-       "-DBUILD_TESTING:BOOL=OFF"
-      "-DTeem_USE_LIB_INSTALL_SUBDIR:BOOL=ON" ;; TODO fix error from build log
-      "-DCMAKE_VERBOSE_MAKEFILE:BOOL=OFF"
-      "-DTeem_PTHREAD:BOOL=OFF"
-      "-DTeem_BZIP2:BOOL=OFF"
-      "-DTeem_ZLIB:BOOL=ON"
-      "-DTeem_PNG:BOOL=OFF"
-      ;; -DZLIB_ROOT:PATH=${ZLIB_ROOT}
-      ;; -DZLIB_INCLUDE_DIR:PATH=${ZLIB_INCLUDE_DIR}
-      ;; -DZLIB_LIBRARY:FILEPATH=${ZLIB_LIBRARY}
-      ;; -DTeem_PNG_DLLCONF_IPATH:PATH=${VTK_DIR}/Utilities
-       )))
+       #:configure-flags (list "-DBUILD_SHARED_LIBS:BOOL=ON"
+                               "-DBUILD_TESTING:BOOL=OFF")))
+    ;; https://discourse.slicer.org/t/unknown-cmake-command-vtkmacrokitpythonwrap-in-snapshot-release-for-vtkteem/13838
+    (inputs (list vtkaddon-slicer))
     (home-page "https://github.com/Slicer/teem/")
     (synopsis
      "A group of libraries for representing, processing and visualizing scientific raster data.")
