1be6f5de802d4187985c577573544c77eb94e55c
Omar El Hajj <omarelhajj97@gmail.com>
2025-03-25 22:30:58 +0000

    [ENH][packages/pythonqt] Remove inheritance from python-pyqt

--- guix-systole/packages/pythonqt.scm
+++ guix-systole/packages/pythonqt.scm
@@ -1,16 +1,17 @@
 (define-module (guix-systole packages pythonqt)
                #:use-module (gnu packages)
-               #:use-module (guix packages)
                #:use-module (gnu packages qt)
-               #:use-module ((guix licenses) #:prefix license:)
+               #:use-module (gnu packages python)
+               #:use-module (guix packages)
                #:use-module (guix download)
-               #:use-module (guix build-system cmake))
+               #:use-module (guix build-system cmake)
+               #:use-module ((guix licenses) #:prefix license:))
 
 (define-public pythonqt-commontk
                (package
-                 (inherit python-pyqt)
+                 ; (inherit python-pyqt)
                  (name "pythonqt-commontk")
-                 ; (version "0.1")
+                 (version "0.1")
                  (source
                    (origin
                      (method url-fetch)
@@ -21,24 +22,24 @@
                     )
                   )
                  (build-system cmake-build-system)
-                 ; (arguments
-                 ;   '(#:tests? #f
-                 ;     #:configure-flags
-                 ;     (list
-                 ;       ; Enable qMRMLWidgets python wrapping
-                 ;       "-DMRMLWidgets_VTK_WRAP_PYTHON:BOOL=ON"
-                 ;
-                 ;       )
-                 ;     )
-                 ;   )
-                 ; (inputs
-                 ;   (list
-                 ;     ; qtbase-5
-                 ;     )
-                 ;   )
-                 ; (home-page "https://github.com/commontk/PythonQt")
-                 ; (synopsis "CMake-ified version of PythonQt")
-                 ; (description "PythonQt is a dynamic Python binding for Qt. It offers an easy way to embed the Python scripting language into your Qt applications.")
+                 (inputs
+                   (list
+                     qtbase-5
+                     python
+                     python-sip
+                     )
+                   )
+                 (arguments
+                   (list
+                     ; #:configure-flags
+                     ; (list
+                     ;   )
+                     #:tests? #f
+                     )
+                   )
+                 (home-page "https://github.com/commontk/PythonQt")
+                 (synopsis "CMake-ified version of PythonQt")
+                 (description "PythonQt is a dynamic Python binding for Qt. It offers an easy way to embed the Python scripting language into your Qt applications.")
                  (license license:lgpl2.1)
                 )
               )
