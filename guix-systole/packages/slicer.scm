bd78b3095d1f964865bf29f5778d7e62e034429e
Rafael Palomar <rafael.palomar@ous-research.no>
2025-12-19 09:59:08 +0100

    [ENH][packages/slicer] Update dependencies
    This commit updates the qt xmlpatterns `qtxmlpatterns` -> `qtxmlpatterns-5`

--- guix-systole/packages/slicer.scm
+++ guix-systole/packages/slicer.scm
@@ -1,19 +1,19 @@
-;; 
+;;
 ;; Copyright @ 2025 Oslo University Hospital
 ;;
 ;; This file is part of SystoleOS.
 ;;
-;; SystoleOS is free software: you can redistribute it and/or modify it under the 
-;; terms of the GNU General Public License as published by the Free Software 
+;; SystoleOS is free software: you can redistribute it and/or modify it under the
+;; terms of the GNU General Public License as published by the Free Software
 ;; Foundation, either version 3 of the License, or (at your option) any later version.
 ;;
-;; SystoleOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+;; SystoleOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ;; PURPOSE. See the GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License along 
+;; You should have received a copy of the GNU General Public License along
 ;; with SystoleOS. If not, see <https://www.gnu.org/licenses/>.
-;; 
+;;
 
 (define-module (guix-systole packages slicer)
   #:use-module ((guix licenses)
@@ -226,7 +226,7 @@ exec ~a --additional-module-path \"$HOME/.guix-profile/lib/Slicer-5.8/SlicerModu
            ;; QT5
            qtbase-5
            qtmultimedia-5
-           qtxmlpatterns
+           qtxmlpatterns-5
            qtdeclarative-5
            qtsvg-5
            qtx11extras
