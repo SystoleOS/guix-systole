1f4bc3b15e5beab4fa9f900eb7ba08ec8608fef2
Rafael Palomar <rafael.palomar@ous-research.no>
2026-02-21 11:07:59 +0100

    [ENH][packages/slicer] Add standalone build patch for Tables module
    Add tables/0001-ENH-Add-standalone-build-support-for-Tables-module.patch
    which injects cmake_minimum_required, find_package(Slicer), UseSlicer.cmake
    include, and the INSTALL_QTLOADABLEMODULES_{LIB,SHARE}_DIR fixups at the
    top of Modules/Loadable/Tables/CMakeLists.txt.

--- systole/systole/packages/slicer.scm
+++ systole/systole/packages/slicer.scm
@@ -506,7 +506,7 @@ bar actor), and a subject hierarchy plugin for color legends.  Built from the
   (make-slicer-loadable-module
    #:name "slicer-tables-5.8"
    #:module-subdir "Tables"
-   #:patches (list)
+   #:patches (list "tables/0001-ENH-Add-standalone-build-support-for-Tables-module.patch")
    #:synopsis "3D Slicer Tables loadable module"
    #:description
    "The Tables loadable module extracted from 3D Slicer.  It provides
