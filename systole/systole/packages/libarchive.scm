3ab96c4050668d958364b1960c8070b4409aa496
Rafael Palomar <rafael.palomar@ous-research.no>
2026-07-17 10:11:53 +0200

    [ENH][packages/libarchive] Update to Slicer fork 3.8.1 (security)
    Bump libarchive-slicer from the April-2022 slicer-v3.6.1 snapshot to
    the slicer-v3.8.1-2025-06-01 branch (30a9657b), the pin upstream
    Slicer requires from 5.10 on.
    
    This is security-relevant staleness: Slicer feeds libarchive remote
    content (sample data downloads, extension archives), and the 3.6.1 ->
    3.8.1 range fixes CVE-2022-36227 plus the 2025 RAR/rar5 series
    (CVE-2025-5914 double-free, CVE-2025-5915 heap overflow,
    CVE-2025-5916, CVE-2025-5918) among others.
    
    Also modernizes the package: git-fetch + git-version instead of an
    autogenerated GitHub tarball, gexp arguments, lint-clean synopsis;
    adds the renamed -DENABLE_PCRE2POSIX switch alongside the old
    spelling.
    
    NOTE: slicer-5.8 and slicer-5.10 need a full rebuild to pick this up;
    schedule on the build hub.

--- systole/systole/packages/libarchive.scm
+++ systole/systole/packages/libarchive.scm
@@ -1,68 +1,87 @@
-;; 
+;;
 ;; Copyright @ 2025 Oslo University Hospital
 ;;
 ;; This file is part of SystoleOS.
 ;;
-;; SystoleOS is free software: you can redistribute it and/or modify it under the 
-;; terms of the GNU General Public License as published by the Free Software 
+;; SystoleOS is free software: you can redistribute it and/or modify it under the
+;; terms of the GNU General Public License as published by the Free Software
 ;; Foundation, either version 3 of the License, or (at your option) any later version.
 ;;
-;; SystoleOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
-;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+;; SystoleOS is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+;; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
 ;; PURPOSE. See the GNU General Public License for more details.
 ;;
-;; You should have received a copy of the GNU General Public License along 
+;; You should have received a copy of the GNU General Public License along
 ;; with SystoleOS. If not, see <https://www.gnu.org/licenses/>.
-;; 
+;;
 
 (define-module (systole packages libarchive)
   #:use-module (guix build-system cmake)
-  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix gexp)
   #:use-module (guix packages)
   #:use-module ((guix licenses)
                 #:prefix license:))
 
 (define-public libarchive-slicer
-  (package
-    (name "libarchive-slicer")
-    (version "3.6.1")
-    (source
-     (origin
-       (method url-fetch)
-       (uri
-        "https://github.com/Slicer/libarchive/archive/14ec55f065e31fbbca23d3d96d43e07f21c6fb6d.tar.gz")
-       (sha256
-        (base32 "1l8a6x0dmvl1q664dzg1f3m92mhbwk8qyf92i4ab8yj720knazvv"))))
-    (build-system cmake-build-system)
-    (arguments
-     `(#:tests? #f
-       #:configure-flags (list "-DCMAKE_C_COMPILER:FILEPATH=gcc"
-                               "-DBUILD_SHARED_LIBS:BOOL=ON"
-                               "-DBUILD_TESTING:BOOL=OFF"
-                               "-DENABLE_ACL:BOOL=OFF"
-                               "-DENABLE_BZip2:BOOL=OFF"
-                               "-DENABLE_CAT:BOOL=OFF"
-                               "-DENABLE_CNG:BOOL=OFF"
-                               "-DENABLE_CPIO:BOOL=OFF"
-                               "-DENABLE_EXPAT:BOOL=OFF"
-                               "-DENABLE_ICONV:BOOL=OFF"
-                               "-DENABLE_LIBB2:BOOL=OFF"
-                               "-DENABLE_LibGCC:BOOL=OFF"
-                               "-DENABLE_LIBXML2:BOOL=OFF"
-                               "-DENABLE_LZ4:BOOL=OFF"
-                               "-DENABLE_LZMA:BOOL=OFF"
-                               "-DENABLE_LZO:BOOL=OFF"
-                               "-DENABLE_MBEDTLS:BOOL=OFF"
-                               "-DENABLE_NETTLE:BOOL=OFF"
-                               "-DENABLE_OPENSSL:BOOL=OFF"
-                               "-DENABLE_PCREPOSIX:BOOL=OFF"
-                               "-DENABLE_TAR:BOOL=OFF"
-                               "-DENABLE_TEST:BOOL=OFF"
-                               "-DENABLE_XATTR:BOOL=OFF"
-                               "-DENABLE_ZSTD:BOOL=OFF"
-                               "-DARCHIVE_CRYPTO_MD5_LIBSYSTEM:BOOL=OFF")))
-    (home-page "https://libarchive.org/")
-    (synopsis "Multi-format archive and compression library.")
-    (description
-     "Libarchive provides a flexible interface for reading and writing archives in various formats such as tar and cpio.  Libarchive also supports reading and writing archives compressed using various compression filters such as gzip and bzip2. The library is inherently stream-oriented; readers serially iterate through the archive, writers serially add things to the archive. In particular, note that there is currently no built-in support for random access nor for in-place modification.  This package provides the `bsdcat', `bsdcpio' and `bsdtar' commands.")
-    (license license:bsd-2)))
+  ;; Slicer/libarchive branch slicer-v3.8.1-2025-06-01, required by
+  ;; Slicer >= 5.10.  Slicer feeds libarchive remote content (sample data,
+  ;; extension archives), and 3.8.1 carries the security fixes accumulated
+  ;; since the previous 3.6.1 pin (CVE-2022-36227, the 2025 RAR series
+  ;; CVE-2025-5914..5918, and friends).
+  (let ((commit "30a9657b6c12f675fd5057cea017b2c9bd924dda")
+        (revision "0"))
+    (package
+      (name "libarchive-slicer")
+      (version (git-version "3.8.1" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/Slicer/libarchive")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32 "0ihf1g7hbg85wvsivzxl264kkkgvq667y8jvnswracv1dlcrcgmf"))))
+      (build-system cmake-build-system)
+      (arguments
+       (list
+        #:tests? #f
+        #:configure-flags #~(list "-DCMAKE_C_COMPILER:FILEPATH=gcc"
+                                  "-DBUILD_SHARED_LIBS:BOOL=ON"
+                                  "-DBUILD_TESTING:BOOL=OFF"
+                                  "-DENABLE_ACL:BOOL=OFF"
+                                  "-DENABLE_BZip2:BOOL=OFF"
+                                  "-DENABLE_CAT:BOOL=OFF"
+                                  "-DENABLE_CNG:BOOL=OFF"
+                                  "-DENABLE_CPIO:BOOL=OFF"
+                                  "-DENABLE_EXPAT:BOOL=OFF"
+                                  "-DENABLE_ICONV:BOOL=OFF"
+                                  "-DENABLE_LIBB2:BOOL=OFF"
+                                  "-DENABLE_LibGCC:BOOL=OFF"
+                                  "-DENABLE_LIBXML2:BOOL=OFF"
+                                  "-DENABLE_LZ4:BOOL=OFF"
+                                  "-DENABLE_LZMA:BOOL=OFF"
+                                  "-DENABLE_LZO:BOOL=OFF"
+                                  "-DENABLE_MBEDTLS:BOOL=OFF"
+                                  "-DENABLE_NETTLE:BOOL=OFF"
+                                  "-DENABLE_OPENSSL:BOOL=OFF"
+                                  ;; 3.8 renamed the PCRE switch (PCRE2); keep
+                                  ;; the old spelling too, it is ignored.
+                                  "-DENABLE_PCREPOSIX:BOOL=OFF"
+                                  "-DENABLE_PCRE2POSIX:BOOL=OFF"
+                                  "-DENABLE_TAR:BOOL=OFF"
+                                  "-DENABLE_TEST:BOOL=OFF"
+                                  "-DENABLE_XATTR:BOOL=OFF"
+                                  "-DENABLE_ZSTD:BOOL=OFF"
+                                  "-DARCHIVE_CRYPTO_MD5_LIBSYSTEM:BOOL=OFF")))
+      (home-page "https://libarchive.org/")
+      (synopsis "Multi-format archive and compression library (Slicer fork)")
+      (description
+       "Libarchive provides a flexible interface for reading and writing
+archives in various formats such as tar and cpio, and reading and writing
+archives compressed with filters such as gzip and bzip2.  The library is
+inherently stream-oriented; readers serially iterate through the archive,
+writers serially add things to the archive.  This package builds Slicer's
+fork, configured as 3D Slicer requires it.")
+      (license license:bsd-2))))
